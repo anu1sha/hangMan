bd74436d29b6355debfc52eb74eb68f167840ffc
Anusha Ghimirey <anusha.ghimirey.2022@student.lhschools.org>
2018-11-26 07:32:05 -0500

    almost

---
+++ src/lickingheights/com/Main.java
@@ -0,0 +1,56 @@
+package lickingheights.com;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        String consent;
+        String secretPhrase;
+
+        System.out.println("Do you want to play Hangman?");
+        consent = keyboard.nextLine();
+        System.out.println("Okay let me explain the rules");
+        System.out.println(" and you will have ");
+        System.out.println("15 tries to guess the letters or the word its self");
+        System.out.println("Are you ready to begin?");
+        consent = keyboard.nextLine();
+        System.out.println("Great! player one can now put in the word and " +
+                "player two may not look");
+        secretPhrase = keyboard.nextLine().toLowerCase();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       System.out.println( "  ______      ");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+
+    }
+}

43083da42ad0f01f7a6d5202ee2f5543ec5a1a12
Anusha Ghimirey <anusha.ghimirey.2022@student.lhschools.org>
2018-11-27 07:39:03 -0500

    there...

--- src/lickingheights/com/Main.java
+++ src/lickingheights/com/Main.java
@@ -7,21 +7,22 @@ public class Main {
 
     public static void main(String[] args) {
 	//
-        Scanner keyboard;
+         Scanner keyboard;
         keyboard = new Scanner(System.in);
         String consent;
         String secretPhrase;
 
-        System.out.println("Do you want to play Hangman?");
-        consent = keyboard.nextLine();
-        System.out.println("Okay let me explain the rules");
-        System.out.println(" and you will have ");
-        System.out.println("15 tries to guess the letters or the word its self");
+        System.out.println("Hello Welcome to my hangman game");
+        System.out.println("Let me explain the game");
+        System.out.println("This is a two person game");
+        System.out.println("The first person enters their phrase");
+        System.out.println("The second person guesses the phrase in less than 15 tries");
         System.out.println("Are you ready to begin?");
         consent = keyboard.nextLine();
         System.out.println("Great! player one can now put in the word and " +
                 "player two may not look");
         secretPhrase = keyboard.nextLine().toLowerCase();
+        String [] guessPhraseBreakdown = secretPhrase.split("");
 
 
 
@@ -38,17 +39,6 @@ public class Main {
 
 
 
-       System.out.println( "  ______      ");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
 
 
 
